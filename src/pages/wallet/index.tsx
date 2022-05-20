37c7a0da073eb9621995a73b7c472400c9a2f2be
alextrindadecosta <alextrindade-costa@outlook.com>
2022-05-20 12:19:10 -0300

    wallet refresh adjusted

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -1,12 +1,23 @@
 import Head from 'next/head'
 import { Summary } from '../../components/Summary'
 import { useCart } from '../../hooks/useCart'
+import { useEffect } from 'react'
 import { NftCartCard } from '../../components/NftCartCard'
 
 import styles from './styles.module.scss'
 
 export default function Wallet() {
-  const { cart } = useCart()
+  const { setCart, cart } = useCart()
+
+  useEffect(() => {
+    const storagedCart = localStorage.getItem('@CryptoStore:cart');
+
+    if (storagedCart && cart.length === 0) {
+      console.log('wallet setCart')
+      setCart(JSON.parse(storagedCart))
+    }
+
+  }, [])
 
   return (
     <>
@@ -16,7 +27,7 @@ export default function Wallet() {
       <div className={styles.container}>
         <div className={styles.walletContainer}>
           <div className={styles.nftsContainer}>
-            {cart.map(nft => <NftCartCard key={nft.id} nft={nft} />)}
+            {cart && cart.map(nft => <NftCartCard key={nft.id} nft={nft} />)}
           </div>
           <Summary />
         </div>
